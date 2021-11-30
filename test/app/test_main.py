693a00cfbdbde61bdf81ee7a6f7029d6497bc759
Alexander Eresov <Alexander.Eresov@origin.com.au>
2021-11-30 09:50:40 +0000

    initial

---
+++ test/app/test_main.py
@@ -0,0 +1,11 @@
+
+from typer.testing import CliRunner
+from app.main import app
+
+runner = CliRunner()
+
+
+def test_app():
+    result = runner.invoke(app, ["goodbye", "Alex", "--formal"])
+    assert result.exit_code == 0
+    assert "Have a good day" in result.stdout

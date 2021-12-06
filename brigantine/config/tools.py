2cb54964879a3f497b38d46a0da1023ea54ea672
Alexander Eresov <Alexander.Eresov@origin.com.au>
2021-12-06 09:56:21 +0000

    wip

--- brigantine/config/tools.py
+++ brigantine/config/tools.py
@@ -5,7 +5,7 @@ from .machine import Machine
 from pathlib import Path
 
 
-def load(filename: Path) -> Machine:
+def load_yaml(filename: Path) -> Machine:
     with filename.open(mode="r", encoding="UTF-8") as file:
         config_data = yaml.safe_load(file)
         return Machine.parse_obj(config_data)
--- tests/brigantine/config/test_tools.py
+++ tests/brigantine/config/test_tools.py
@@ -1,8 +1,8 @@
 from pathlib import Path
-from brigantine.config.tools import load
+from brigantine.config.tools import load_yaml
 import pytest
 
 
 @pytest.mark.parametrize("filename", [Path("tests/assets/config_good.yaml")])
 def test_load(filename: Path):
-    machine = load(filename)
+    machine = load_yaml(filename)
